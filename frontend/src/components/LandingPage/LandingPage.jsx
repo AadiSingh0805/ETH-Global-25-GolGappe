56821ab793e16c89f2835fe319b56a901fa360ac
AadiSingh0805 <134000320+AadiSingh0805@users.noreply.github.com>
2025-09-27 12:07:46 +0530

    Name change for Creators - Orgs

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -31,7 +31,7 @@ const LandingPage = () => {
                 <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
               </svg>
             </div>
-            <h3 className="option-title">Creator</h3>
+            <h3 className="option-title">Organization</h3>
             <p className="option-description">
               Post bounties, manage repositories, and find talented contributors for your projects
             </p>
